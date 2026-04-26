2288ef6f7e94ea0859feeac50d116ed099857d3a
DethPlague98 <tnaes98@gmail.com>
2026-04-26 00:25:04 -0500

    initial project

--- create-array2.js
+++ create-array2.js
@@ -1,1 +1,15 @@
-//Begin
+//Task 1 Initialize
+let arr1 = Array(7).fill("Hello");
+console.log(arr1);
+
+//Task 2 Update the Array
+arr1.fill("17",1,5);
+console.log(arr1);
+
+//Task 3 Populate with a for loop
+let arr2 = Array(5);
+
+for (i = 0; i < arr2.length; i++){
+    arr2[i] = (i *10);
+    console.log(arr2[i]);
+} 
